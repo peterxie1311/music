f6f7cca025bc12f9f518e7634d30a9bf24a72942
Peter Xie <peterxie@Peters-MacBook-Pro.local>
2024-03-05 20:48:22 +1000

    First commit

---
+++ music/music/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  music
+//
+//  Created by Peter Xie on 5/3/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ music/music/musicApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicApp.swift
+//  music
+//
+//  Created by Peter Xie on 5/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct musicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
